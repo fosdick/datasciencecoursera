25606d724fb64b2ed24850ee8f4c155ced297b4c
Jarvis Fosdick <jfosdick@dcpmidstream.com>
2018-03-02 17:20:00 -0700

    update codebook with variable def

--- c3/Practical/run_analysis.R
+++ c3/Practical/run_analysis.R
@@ -1,4 +1,6 @@
 ## opens the Rmd file and makes the code book.
 ## Rmd file is R markdown
-knit("runAnalysisCodebook.Rmd", output="codebook.md", encoding="ISO8859-1", quiet=TRUE)
-markdownToHTML("codebook.md", "codebook.html")
+knit("runAnalysisCodebook.Rmd", output="runAnalysisCodebook.md", encoding="ISO8859-1", quiet=TRUE)
+markdownToHTML("runAnalysisCodebook.md", "runAnalysisCodebook.html")
+
+knit("makeCodebook.Rmd", output="codebook.md", encoding="ISO8859-1", quiet=TRUE)
